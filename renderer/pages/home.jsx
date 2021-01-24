be5d967a0d9a590296a73085eafc6bf316be0a90
aoao <nao.iwakura.lain.sheddy@gmail.com>
2021-01-24 16:23:41 +0900

    -

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,24 +1,26 @@
-import React, {useEffect} from 'react';
-import Head from 'next/head';
-import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
-import Typography from '@material-ui/core/Typography';
-import Link from '../components/Link';
+import React, {useState, useEffect} from 'react';
 import {HeadThreeColumnFoot, Header, Left, Center, Right, Footer} from '../components/layouts/HeadThreeColumnFoot'
-const fs = require('fs');
-import {mockdata} from '../lib/database.mock.js'
-const currentColumnSpaceObject = mockdata[0]
+import {readFile, copyFile, writeFile} from 'fs'
+import { v4 as uuidv4 } from 'uuid'
+const databaseFilePath = "userdata/db/database.json"
 
 
 const Home = () => {
 
-  const currentColumnSpaceUUID = "test_column_space"    //仮のモック
+  const [columnspaceDB, setColumnspaceDB] = useState(null)
 
+  const currentColumnSpaceUUID = "test_column_space"    //仮のモック
+  const currentMainDisplayedColumnUUID = "test_file_column_uuid"
+  const currentMainDisplayedColumnDatas = (columnspaceDB !== null) ? columnspaceDB[currentColumnSpaceUUID].columns[currentMainDisplayedColumnUUID].datas : null;
+
+  // DBの読み込み
+  useEffect(() => {
+    readFile(databaseFilePath, "utf8", (error, databaseText) => {
+      setColumnspaceDB(JSON.parse(databaseText))
+    })
+  }, [])
+
+  // D&Dの制御
   useEffect(() => {
 
     document.ondragover = document.ondrop = (e) => {
@@ -37,19 +39,52 @@ const Home = () => {
         リストアイテムの表示は軽くすること
         アップロード完了したらファイルのパスをDBに保存すること（そしてメモリに展開すること）（single truth of source的なものも実現させたいところ…）
         そしてリビルドさせること
+        ひとまずはmainDisplayedColumnにアップロードさせるが、後で他カラムに使うファイルのアップロードにも対応させること
+        ひとまずメモリ上でjson型のDBを作り、定期保存かつ、windowが閉じられた時に保存するような仕様にする
+        asyncだと後々難しい場合、synkで全部やるのも考える
       */
       const fileList = e.dataTransfer.files
       Array.from(fileList).forEach(fileObject => {
-        fs.copyFileSync(fileObject.path, `userdata/${currentColumnSpaceUUID}/${fileObject.name}`, (error) => {
+        const savePath = `userdata/column_spaces/${currentColumnSpaceUUID}/${currentMainDisplayedColumnUUID}/${fileObject.name}`
+        copyFile(fileObject.path, savePath, (error) => {
           if (error) {
-            console.log(err.stack)
+            console.log(error.stack)
             return
           }
-          console.log('Done.')
+
+          const newColumnSpaceDB = Object.assign({}, columnspaceDB)
+          const newFileUUID = uuidv4()
+          console.log(columnspaceDB)
+          newColumnSpaceDB[currentColumnSpaceUUID].columns[currentMainDisplayedColumnUUID].datas[newFileUUID] = {
+            path: savePath,
+            type: fileObject.type,
+            name: fileObject.name,
+            childs_columns_datas: {
+              test_technique_column_uuid: {}
+            }
+          }
+
+          setColumnspaceDB((currentState) =>  newColumnSpaceDB);
+          console.log('ファイル取り込み完了')
+
+          writeFile(databaseFilePath, JSON.stringify(newColumnSpaceDB, null, "\t"), "utf8", (error) => {
+            if (error) {
+              console.log(error.stack)
+              return
+            }
+
+            console.log("DB書き出し完了")
+          })
         })
       })
     }
-  }, [])
+  }, [columnspaceDB])
+
+  if (columnspaceDB === null) {
+    return (
+      <div>DB読込中</div>
+    )
+  }
 
   return (
     <React.Fragment>
@@ -57,11 +92,12 @@ const Home = () => {
         <Header>header</Header>
         <Left>left</Left>
         <Center>
-          {Object.keys(mockdata[currentColumnSpaceUUID].medias).map((mediaUUID) => {
-            const media = mockdata[currentColumnSpaceUUID].medias[mediaUUID]
-            return <div>{media.name}</div>
-          })
-        }
+          {
+            Object.keys(currentMainDisplayedColumnDatas).map((dataUUID, index) => {
+              const data = currentMainDisplayedColumnDatas[dataUUID]
+              return <div key={`${data.name}-${index}`}>{data.name}</div>
+            })
+          }
         </Center>
         <Right>right</Right>
         <Footer>footer</Footer>
