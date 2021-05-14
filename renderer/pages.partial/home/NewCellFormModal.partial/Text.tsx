a4f6f8254c34bfafd1c46fa174a70a61d20628fe
aoao <nao.iwakura.lain.sheddy@gmail.com>
2021-05-15 04:52:26 +0900

    -

--- renderer/pages.partial/home/NewCellFormModal.partial/Text.tsx
+++ renderer/pages.partial/home/NewCellFormModal.partial/Text.tsx
@@ -36,6 +36,7 @@ export const NewCellFormModalBodyText: React.FC<Props> = (props) => {
         <Button type="submit" isDisabled={!(formik.isValid && formik.dirty)} colorScheme="blue" mr={3} >作成</Button>
         <Button variant="ghost" onClick={props.handleClickNewCellFormClose}>キャンセル</Button>
       </div>
+      {/* TODO たぶん関連づけの操作もこのモーダル上でできるようにしたほうがいいと思う */}
     </form>
   )
 }
