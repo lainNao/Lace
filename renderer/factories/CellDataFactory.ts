2c2d4bbaba5af786faf02058234be202fea42d5e
aoao <nao.iwakura.lain.sheddy@gmail.com>
2021-05-16 05:04:53 +0900

    -

--- renderer/factories/CellDataFactory.ts
+++ renderer/factories/CellDataFactory.ts
@@ -1,12 +1,14 @@
-import { CellData } from "../models/CellData";
-import { BooleanCellData } from "../models/CellData.implemented/BooleanCellData";
-import { ImageCellData } from "../models/CellData.implemented/ImageCellData";
-import { MarkdownCellData } from "../models/CellData.implemented/MarkdownCellData";
-import { RadioCellData } from "../models/CellData.implemented/RadioCellData";
-import { SoundCellData } from "../models/CellData.implemented/SoundCellData";
-import { TextCellData } from "../models/CellData.implemented/TextCellData";
-import { VideoCellData } from "../models/CellData.implemented/VideoCellData";
 import { CellDataType } from "../resources/CellDataType";
+import { CellData } from "../models/CellData";
+import {
+  BooleanCellData,
+  ImageCellData,
+  MarkdownCellData,
+  RadioCellData,
+  SoundCellData,
+  TextCellData,
+  VideoCellData,
+} from "../models/CellData.implemented"
 
 export class CellDataFactory {
 
