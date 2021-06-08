619a60320c7db0665ba9925fda8e54fcb17c2b04
aoao <nao.iwakura.lain.sheddy@gmail.com>
2021-06-09 08:12:59 +0900

    補足ペインのloading対応

--- renderer/pages.partial/home/ColumnSpaceDisplayer.partial/SubPane.tsx
+++ renderer/pages.partial/home/ColumnSpaceDisplayer.partial/SubPane.tsx
@@ -1,4 +1,4 @@
-import { Cell, ColumnSpace } from "../../../models/ColumnSpaces"
+import { Cell, Column, ColumnSpace } from "../../../models/ColumnSpaces"
 import { DisplaySetting } from "../../../models/DisplaySettings"
 import { Tag } from "@chakra-ui/react"
 import { CellDataType, cellDataTypeIcons } from "../../../resources/CellDataType"
@@ -7,7 +7,8 @@ import { CellViewerHorizontal } from "../../../components/CellViewerHorizontal"
 import { useRecoilState } from "recoil"
 import relatedCellsState from "../../../recoils/atoms/relatedCellsState"
 import { RelatedCellDisplayDirectionType } from "../../../models/DisplaySettings/RelatedCellsDisplaySetting"
-import React, { useEffect, useMemo } from "react"
+import React, { useEffect, useState } from "react"
+import ReactLoading from "react-loading";
 
 type Props = {
   className: string;
@@ -22,7 +23,6 @@ type Props = {
 
 // TODO これ、onmouseが変わる度に新しく内部を生成するのかなりのコストな気がするので（高速でバンバンやられうる。CPUもったいない）、キャッシュをどうにか作ろうかな…キーをセルIDと表示設定IDにして、どちらかに変化あったらキャッシュを更新
 export const SubPane = (props: Props) => {
-  const [relatedCells, setRelatedCells] = useRecoilState(relatedCellsState);
 
   if (!props.targetCell) {
     //TODO ここもう少しどうにかする
@@ -37,11 +37,6 @@ export const SubPane = (props: Props) => {
 
     return selectedColumnIds.includes(column.id);
   });
-
-  // TODO なぜか以下やるとエラーになる（Rendered more hooks than during the previous render）
-  // useEffect(() => {
-  //   console.log(Date.now().toString)
-  // }, [])
 
   return (
     <div className={`${props.className}`}>
@@ -65,84 +60,147 @@ export const SubPane = (props: Props) => {
       <div className="mb-3">
         <Tag colorScheme="cyan">関連セル</Tag>
       </div>
-      <div className="">
-        {subColumns.map(subColumn => {
-          // TODO メモ化とかも考えたけどpropsが多すぎて再レンダリング必要かどうかの比較するのも計算コストかかりそうだったので一旦略してる
-          // 中央ペインでonmouseしているセルIDと関連しているセルだけフィルタリング。これがそれなりにたぶんコスト高い
-          const currentRelatedCells = subColumn.cells.children
-            .filter(cell =>
-              relatedCells.isRelated(props.columnSpace.id,
-                { columnId: subColumn.id, cellId: cell.id },
-                { columnId: props.displaySetting.mainColumn, cellId: props.targetCell.id }
-              )
-            );
+      <SubColumns
+        subColumns={subColumns}
+        columnSpace={props.columnSpace}
+        displaySetting={props.displaySetting}
+        targetCell={props.targetCell}
+        onSoundCellToggle={props.onSoundCellToggle}
+        onSoundCellPlay={props.onSoundCellPlay}
+        onSoundCellPause={props.onSoundCellPause}
+        onVideoCellToggle={props.onVideoCellToggle}
+      />
 
-          const currentColumnDisplaySetting = props.displaySetting.relatedCellsDisplaySettings.find(displaySetting => displaySetting.columnId === subColumn.id)
+    </div>
+  )
+}
 
-          return (
-            <div key={subColumn.id} className="mb-4">
+type SubColumnsProps = {
+  subColumns: Column[],
+  columnSpace: ColumnSpace,
+  displaySetting: DisplaySetting,
+  targetCell: Cell,
+  onSoundCellToggle: (event) => void,
+  onSoundCellPlay: (event) => void,
+  onSoundCellPause: (event) => void,
+  onVideoCellToggle: (event) => void,
+}
 
-              {/* カラム名 */}
-              <div className="font-bold flex items-center">
-                <span>{cellDataTypeIcons(subColumn.type, "w-3 h-3 mr-2")}</span>
-                <span className="font-sans text-blue-400 text-sm">{subColumn.name}</span>
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+//TODO ここの表示は非同期にしたい
+const SubColumns = (props: SubColumnsProps) => {
+  const [relatedCells, setRelatedCells] = useRecoilState(relatedCellsState);
+  const [relatedCellsAndColumnSpaceSettingArray , setRelatedCellsAndColumnSpaceSettingArray] = useState(null);
+
+  useEffect(() => {
+    (async() => {
+      const getRelatedCellsAndColumnSpaceSettingArray = () => new Promise(async(resolve, reject) => {
+        setImmediate(async() => {
+          const targetDatas = props.subColumns.map(subColumn => {
+            return {
+              relatedCells: subColumn.cells.children.filter(cell =>
+                  relatedCells.isRelated(props.columnSpace.id,
+                    { columnId: subColumn.id, cellId: cell.id },
+                    { columnId: props.displaySetting.mainColumn, cellId: props.targetCell.id }
+                  )
+                ),
+              displaySetting: props.displaySetting.relatedCellsDisplaySettings.find(displaySetting => displaySetting.columnId === subColumn.id)
+            }
+          })
+          resolve(targetDatas);
+        })
+      })
+      // 一旦初期化
+      setRelatedCellsAndColumnSpaceSettingArray(null);
+      const result = await getRelatedCellsAndColumnSpaceSettingArray();
+      // 読み込み結果を反映
+      setRelatedCellsAndColumnSpaceSettingArray(result);
+    })();
+  }, [props.targetCell.id]);
+
+  // まだ読み込みが完了してない場合
+  if (!relatedCellsAndColumnSpaceSettingArray) {
+    return (
+      <div className="w-full">
+        <ReactLoading type="bubbles" />
+      </div>
+    )
+  }
+
+  return (
+    <div className="">
+      {props.subColumns.map((subColumn, currentColumnIndex) => {
+        // TODO メモ化とかも考えたけどpropsが多すぎて再レンダリング必要かどうかの比較するのも計算コストかかりそうだったので一旦略してる
+        // 中央ペインでonmouseしているセルIDと関連しているセルだけフィルタリング。これがそれなりにたぶんコスト高い
+
+        return (
+          <div key={subColumn.id} className="mb-4">
+
+            {/* カラム名 */}
+            <div className="font-bold flex items-center">
+              <span>{cellDataTypeIcons(subColumn.type, "w-3 h-3 mr-2")}</span>
+              <span className="font-sans text-blue-400 text-sm">{subColumn.name}</span>
+            </div>
+
+            {/* 関連セルが無い場合 */}
+            {relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].relatedCells.length === 0 && (
+              <div className="ml-8">
+                <div className="select-none">-</div>
               </div>
+            )}
 
-              {/* 関連セルが無い場合 */}
-              {currentRelatedCells.length === 0 && (
-                <div className="ml-8">
-                  <div className="select-none">-</div>
-                </div>
-              )}
+            {/* 関連セルがあり、Verticalの場合 */}
+            {(relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].relatedCells.length !== 0 && relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].displaySetting.direction === RelatedCellDisplayDirectionType.Vertical) && (
+              <div className="ml-8 mt-1">
+                {relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].relatedCells.map(cell => {
+                  return (
+                    <CellViewer
+                      key={cell.id}
+                      className="mb-2"
+                      cell={cell}
+                      withLiPrefix={cell.type === CellDataType.Text}
+                      onSoundCellToggle={props.onSoundCellToggle}
+                      onSoundCellPlay={props.onSoundCellPlay}
+                      onSoundCellPause={props.onSoundCellPause}
+                      onVideoCellToggle={props.onVideoCellToggle}
+                    />
+                  )
+                })}
+              </div>
+            )}
 
-              {/* 関連セルがあり、Verticalの場合 */}
-              {(currentRelatedCells.length !== 0 && currentColumnDisplaySetting.direction === RelatedCellDisplayDirectionType.Vertical) && (
-                <div className="ml-8 mt-1">
-                  {currentRelatedCells.map(cell => {
-                    return (
-                      <CellViewer
+            {/* 関連セルがあり、Horizontalの場合 */}
+            {(relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].relatedCells.length !== 0 && relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].displaySetting.direction === RelatedCellDisplayDirectionType.Horizontal) && (
+              <div className="ml-8 mt-1">
+                {relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].relatedCells.map((cell, index) => {
+                  return (
+                    <React.Fragment key={cell.id}>
+                      <CellViewerHorizontal
+                        tooltipId={index + "-" + subColumn.id + "-"+ cell.id}   //TODO ここもう少しどうにかする　本当はindexの代わりに表示設定IDとかほしいところ　いずれキャッシュ制御で生成するようにするなら表示設定IDも必要になるだろうし
+                        displayType={relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].displaySetting.hListDisplayType}
                         key={cell.id}
                         className="mb-2"
                         cell={cell}
-                        withLiPrefix={cell.type === CellDataType.Text}
-                        onSoundCellToggle={props.onSoundCellToggle}
-                        onSoundCellPlay={props.onSoundCellPlay}
-                        onSoundCellPause={props.onSoundCellPause}
-                        onVideoCellToggle={props.onVideoCellToggle}
+                        withBackgroundHoveredColor={true}
                       />
-                    )
-                  })}
-                </div>
-              )}
+                      {index !== relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].relatedCells.length-1 &&
+                        <span className="mx-1 select-none">{relatedCellsAndColumnSpaceSettingArray[currentColumnIndex].displaySetting.hListSeparator}</span>
+                      }
+                    </React.Fragment>
+                  )
+                })}
+              </div>
+            )}
 
-              {/* 関連セルがあり、Horizontalの場合 */}
-              {(currentRelatedCells.length !== 0 && currentColumnDisplaySetting.direction === RelatedCellDisplayDirectionType.Horizontal) && (
-                <div className="ml-8 mt-1">
-                  {currentRelatedCells.map((cell, index) => {
-                    return (
-                      <React.Fragment key={cell.id}>
-                        <CellViewerHorizontal
-                          tooltipId={index + "-" + subColumn.id + "-"+ cell.id}   //TODO ここもう少しどうにかする　本当はindexの代わりに表示設定IDとかほしいところ　いずれキャッシュ制御で生成するようにするなら表示設定IDも必要になるだろうし
-                          displayType={currentColumnDisplaySetting.hListDisplayType}
-                          key={cell.id}
-                          className="mb-2"
-                          cell={cell}
-                          withBackgroundHoveredColor={true}
-                        />
-                        {index !== currentRelatedCells.length-1 &&
-                          <span className="mx-1 select-none">{currentColumnDisplaySetting.hListSeparator}</span>
-                        }
-                      </React.Fragment>
-                    )
-                  })}
-                </div>
-              )}
-
-            </div>
-          )
-        })}
-      </div>
-
+          </div>
+        )
+      })}
     </div>
+
+
   )
 }
