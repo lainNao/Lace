55a532442b453bf26839e05f68a2293acf1bad14
aoao <nao.iwakura.lain.sheddy@gmail.com>
2021-06-15 11:40:54 +0900

    -

--- renderer/usecases/createCellUsecase.partial/createVideoCellUsecase.ts
+++
@@ -1,8 +0,0 @@
-import { ColumnSpaces } from "../../models/ColumnSpaces";
-import { CreateCellUsecasesArgs } from "../createCellUsecase";
-
-export const createVideoCellUsecase = async(args: CreateCellUsecasesArgs): Promise<ColumnSpaces> => {
-  console.log(args.cellData)
-  throw new Error("未実装です");
-  return null;
-}
